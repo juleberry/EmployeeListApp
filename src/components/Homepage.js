43aaec0b678579a073a6510a408142a4a6aaf4ce
juleberry <105573843+juleberry@users.noreply.github.com>
2022-09-28 14:04:26 -0400

    initial setup

---
+++ src/components/Homepage.js
@@ -0,0 +1,10 @@
+export default (props) => {
+  return (
+    <>
+    <h1>Employee Directory</h1>
+    <h2>search bar -- empty field</h2>
+    <h3>employee list -- will contain employee list items</h3>
+    <h4>employee list items</h4>
+    </>
+  )
+}
