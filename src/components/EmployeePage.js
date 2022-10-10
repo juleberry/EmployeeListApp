dad3a24519b13b8b76ca0312cb84bff5fc8c427b
juleberry <105573843+juleberry@users.noreply.github.com>
2022-10-09 23:08:34 -0400

    start of employee page

---
+++ src/components/EmployeePage.js
@@ -0,0 +1,17 @@
+export default function EmployeePage (props) {
+  return (
+    <>
+      <div id="empPageHeader"><button id="backBtn">Back</button>
+      <h1>Employee</h1>
+      </div>
+      <div>
+      <h2>Employee Name</h2>
+      <img src="https://thumbs2.imgbox.com/cd/87/jnDoymdD_t.png" id="silhouette" />
+      <h3>Employee Title</h3>
+      <h4>Employee Office Phone</h4>
+      <h4>Employee Mobile Phone</h4>
+      <h4>Employee Email</h4>
+      </div>
+    </>
+  )
+}
--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,14 +1,20 @@
 import EmployeeList from "./EmployeeList"
 import Header from "./Header"
 import SearchBar from "./SearchBar"
+import EmployeePage from "./EmployeePage"
 
 const Homepage = (props) => {
   return (
   <>
-    <div class="section">
-      <Header />
-      <SearchBar />
-      <EmployeeList />
+    <div id="homeContainer">
+      <div class="section" id="leftSide">
+        <Header />
+        <SearchBar />
+        <EmployeeList />
+      </div>
+      <div class="section" id="rightSide">
+        <EmployeePage />
+      </div>
     </div>
   </>
   )
